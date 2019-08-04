796f191ebeeedfacb0dd2cb4cb9a31679308e54d
Hendrik Brandt <heb@20khz.de>
2019-08-04 21:28:53 +1000

    Update scripts to Proton 4.11

--- games/TheElderScrollsOnline-Linux/start-eso-installer.sh
+++ games/TheElderScrollsOnline-Linux/start-eso-installer.sh
@@ -4,5 +4,5 @@ PROTON_NO_ESYNC=1 \
 mesa_glthread=true \
 vblank_mode=0 \
 STEAM_COMPAT_DATA_PATH=~/.proton python3 \
-	~/.steam/steam/steamapps/common/Proton\ 4.2/proton waitforexitandrun \
+	~/.steam/steam/steamapps/common/Proton\ 4.11/proton waitforexitandrun \
 	~/Downloads/Install_ESO.exe
--- games/TheElderScrollsOnline-Linux/start-eso-launcher.sh
+++ games/TheElderScrollsOnline-Linux/start-eso-launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 STEAM_COMPAT_DATA_PATH=~/.proton python3 \
-	~/.steam/steam/steamapps/common/Proton\ 4.2/proton waitforexitandrun \
+	~/.steam/steam/steamapps/common/Proton\ 4.11/proton waitforexitandrun \
 	~/.proton/pfx/drive_c/Program\ Files\ \(x86\)/Zenimax\ Online/Launcher/Bethesda.net_Launcher.exe
